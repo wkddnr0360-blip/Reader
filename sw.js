27e2f485de569ff8f09ba18cfc6a455f08249cdc
wkddnr0360-blip <wkddnr0360@gmail.com>
2026-05-07 05:52:53 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE_NAME = 'pacemaker-v2';
+const CACHE_NAME = 'reader-v1';
+
 // 깃허브 페이지 경로에 맞춰 캐싱할 파일 목록 지정
-const urlsToCache = [
+const urlsToCache =[
     '/Reader/',
     '/Reader/index.html',
     '/Reader/manifest.json',
@@ -34,7 +35,7 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
 
-// 3. 인터넷이 끊겼을 때 캐시된 파일 제공
+// 3. 오프라인에서도 캐시된 파일 제공
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         fetch(event.request).catch(() => caches.match(event.request))
